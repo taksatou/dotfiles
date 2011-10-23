78a139700c5d6a5a6f2ac2331aa06603698bd9a1
takayuki <takayuki.sato.dev@gmail.com>
2011-10-23 19:06:02 +0900

    init script

---
+++ init-env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OLD_DOTFILES=$HOME/.old_dotfiles.d
+DOTFILES='.emacs.d  .gitconfig  .vimrc .emacs .tmux.conf .zshrc'
+
+PWD=`pwd`
+BACKUP_DIR=$OLD_DOTFILES/`date +%s`
+mkdir -p $BACKUP_DIR
+
+
+for i in $DOTFILES; do
+    if [ ! -e $PWD/$i ]; then
+	echo "$i doesn't exist"
+	exit 1
+    fi
+done
+    
+for i in $DOTFILES; do
+    if [ -e $HOME/$i ]; then
+	mv $HOME/$i $BACKUP_DIR/$i
+    fi
+    ln -s $PWD/$i $HOME/$i
+done
