65cae485993993abc01fe4ecd7d6a26411c0a881
takayuki <takayuki.sato.dev@gmail.com>
2011-10-31 02:30:33 +0900

    modified script

--- init-env.sh
+++ init-env.sh
@@ -16,7 +16,7 @@ for i in $DOTFILES; do
 done
     
 for i in $DOTFILES; do
-    if [ -e $HOME/$i ]; then
+    if [ -e $HOME/$i -o -L $HOME/$i ]; then
 	mv $HOME/$i $BACKUP_DIR/$i
     fi
     ln -s $PWD/$i $HOME/$i
