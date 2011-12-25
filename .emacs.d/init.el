d0f9e24d75ffad21341f520c275d2050bed40518
takayuki <takayuki.sato.dev@gmail.com>
2011-12-26 01:41:02 +0900

    (setq frame-background-mode 'dark)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,6 +42,7 @@
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
+(setq frame-background-mode 'dark)
 (setq visible-bell nil)
 (setq ring-bell-function 'ignore)
 (setq column-number-mode t)
