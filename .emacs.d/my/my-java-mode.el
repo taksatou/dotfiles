b5103fb10ddaee646c6c6256a8b679f56da0cdbd
takayuki <takayuki.sato.dev@gmail.com>
2012-08-21 15:22:31 +0900

    fixed javamode

--- .emacs.d/my/my-java-mode.el
+++ .emacs.d/my/my-java-mode.el
@@ -2,14 +2,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; java
 ;;(require 'jde)
-(add-to-list 'load-path "~/.emacs.d/malabar-1.4.0/lisp")
 
-(require 'cedet)
-;(semantic-load-enable-minimum-features) ;; or enable more if you wish
-(require 'malabar-mode)
+(use 'cedet
+     (add-to-list 'load-path "~/.emacs.d/malabar-1.4.0/lisp")
 
-(setq malabar-groovy-lib-dir "~/.emacs.d/malabar-1.4.0/lib")
-(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+                                        ;(semantic-load-enable-minimum-features) ;; or enable more if you wish
+     (require 'malabar-mode)
+
+     (setq malabar-groovy-lib-dir "~/.emacs.d/malabar-1.4.0/lib")
+     (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode)))
 
 (provide 'my-java-mode)
 
