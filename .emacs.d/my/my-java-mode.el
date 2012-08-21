3f9aca73e3db18727c20e730e61773591c83059a
Takayuki Sato <takayuki.sato@gree.co.jp>
2012-08-21 15:23:47 +0900

    java-mode

--- .emacs.d/my/my-java-mode.el
+++ .emacs.d/my/my-java-mode.el
@@ -5,6 +5,7 @@
 (add-to-list 'load-path "~/.emacs.d/malabar-1.4.0/lisp")
 
 (require 'cedet)
+(semantic-mode)
 ;(semantic-load-enable-minimum-features) ;; or enable more if you wish
 (require 'malabar-mode)
 
