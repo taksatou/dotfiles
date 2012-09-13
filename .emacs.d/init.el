e15809aa6f2f2a5c1f0de9b5b828e34f1588adee
Takayuki Sato <takayuki.sato@gree.co.jp>
2012-09-13 10:31:47 +0900

    flymake, all.el, tail.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -332,8 +332,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ffap settings
-(ffap-bindings)
-(setq ffap-kpathsea-depth 5)
+;; (ffap-bindings)
+;; (setq ffap-kpathsea-depth 5)
 (setq ff-other-file-alist
       '(("\\.mm$" (".h"))
         ("\\.m$" (".h"))
@@ -357,26 +357,32 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; flymake
-;; flymake 現在行のエラーをpopup.elのツールチップで表示する
-(defun flymake-display-err-menu-for-current-line ()
-  (interactive)
-  (let* ((line-no             (flymake-current-line-no))
-         (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no))))
-    (when line-err-info-list
-      (let* ((count           (length line-err-info-list))
-             (menu-item-text  nil))
-        (while (> count 0)
-          (setq menu-item-text (flymake-ler-text (nth (1- count) line-err-info-list)))
-          (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
-                 (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
-            (if file
-                (setq menu-item-text (concat menu-item-text " - " file "(" (format "%d" line) ")"))))
-          (setq count (1- count))
-          (if (> count 0) (setq menu-item-text (concat menu-item-text "\n")))
-          )
-        (popup-tip menu-item-text)))))
+(use 'flymake
+     (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+       (setq flymake-check-was-interrupted t))
+     (ad-activate 'flymake-post-syntax-check)
 
-(global-set-key "\C-c\C-m" 'flymake-display-err-menu-for-current-line)
+     ;; flymake 現在行のエラーをpopup.elのツールチップで表示する
+     (defun flymake-display-err-menu-for-current-line ()
+       (interactive)
+       (let* ((line-no             (flymake-current-line-no))
+              (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no))))
+         (when line-err-info-list
+           (let* ((count           (length line-err-info-list))
+                  (menu-item-text  nil))
+             (while (> count 0)
+               (setq menu-item-text (flymake-ler-text (nth (1- count) line-err-info-list)))
+               (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
+                      (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
+                 (if file
+                     (setq menu-item-text (concat menu-item-text " - " file "(" (format "%d" line) ")"))))
+               (setq count (1- count))
+               (if (> count 0) (setq menu-item-text (concat menu-item-text "\n")))
+               )
+             (popup-tip menu-item-text)))))
+
+     (global-set-key "\C-c\C-m" 'flymake-display-err-menu-for-current-line)
+     )
 
 ;; (use 'csharp-mode
 ;;      (setq auto-mode-alist
@@ -402,3 +408,6 @@
 (use 'my-java-mode)
 (use 'open-junk-file)
 (use 'summarye)
+
+(use 'all)
+(use 'tail)
