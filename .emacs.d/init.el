bec5c828ca87e39b9e9a8a6316e85f547e3300c2
takayuki <takayuki.sato.dev@gmail.com>
2013-04-13 15:31:09 +0900

    utf-8

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,7 +47,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; base settings
 ;;;;;;;;;;;;;;;;;;;;;;
-(tool-bar-mode 0)
+(if (boundp 'tool-bar-mode)
+    (tool-bar-mode 0))
+
 
 (if (boundp 'scroll-bar-mode)
     (scroll-bar-mode 0))
@@ -75,7 +77,8 @@
 
 (cond (window-system
        (setq x-select-enable-clipboard t)
-       (setenv "LC_ALL" "en_US.UTF-8")))
+       ;(setenv "LC_ALL" "en_US.UTF-8")
+       ))
 
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
