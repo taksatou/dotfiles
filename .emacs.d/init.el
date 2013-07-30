ce885dbaee02164e7f5eb14112536b3bef5842bf
taksatou <takayuki.sato.dev@gmail.com>
2013-07-30 17:35:02 +0900

    multiple-cursor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -667,6 +667,15 @@
      (global-set-key (kbd "C-c C-SPC") 'ace-jump-mode))
 
 
+(use 'multiple-cursors
+     (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+     (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+     (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+     (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+     
+     )
+
+
 ;;
 ;; should be the last
 ;;
