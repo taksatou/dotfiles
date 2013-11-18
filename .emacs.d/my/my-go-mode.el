f738ff74581273acac71c371d6b4b4d2bcfc0233
takayuki <takayuki.sato.dev@gmail.com>
2013-11-18 10:54:45 +0900

    fixed go mode

--- .emacs.d/my/my-go-mode.el
+++ .emacs.d/my/my-go-mode.el
@@ -3,8 +3,12 @@
 
 (add-hook 'go-mode-hook
           (lambda ()
+            (setq indent-tabs-mode t)
             (local-set-key (kbd "M-.") 'godef-jump)
             (local-set-key (kbd "C-c ;") 'go-import-add)))
+(add-hook 'before-save-hook
+		  (lambda ()
+			(gofmt-before-save)))
 
 ;; flymake
 (add-to-list 'load-path (format "%s/src/github.com/dougm/goflymake" (getenv "GOPATH")))
